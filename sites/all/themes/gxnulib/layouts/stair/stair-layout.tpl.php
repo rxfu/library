6ddcc399d939c8379cca057a146f7599cf3843e2
Stephen Foo <stephen.frx@gmail.com>
2015-04-02 23:20:30 +0800

    修改gxnulib主题stair布局和字号

--- sites/all/themes/gxnulib/layouts/stair/stair-layout.tpl.php
+++ sites/all/themes/gxnulib/layouts/stair/stair-layout.tpl.php
@@ -1,35 +1,63 @@
-<div<?php print $attributes; ?>>
+<div class="l-page"<?php print $attributes; ?>>
+    <?php print render($page['bar']); ?>
     <header class="l-header" role="banner">
-        <div class="l-constrained">
-            <?php print render($page['bar']); ?>
-        </div>
         <div class="l-constrained">
             <div class="l-branding site-branding">
                 <?php if ($logo): ?>
                 <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="site-branding__logo">
                     <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>">
                 </a>
-                <?php endif; ?>
-                <?php if ($site_name): ?>
-                <a href="<?php print $site_name; ?>" title="<?php print t('Home') ?>" rel="home" class="site-branding__name">
-                    <span><?php print $site_name; ?></span>
-                </a>
-                <?php endif; ?>
-                <?php if ($site_slogan): ?>
-                <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
+                <?php elseif ($site_name || $site_slogan): ?>
+                    <?php if ($site_name): ?>
+                        <a href="<?php print $site_name; ?>" title="<?php print t('Home') ?>" rel="home" class="site-branding__name">
+                            <span><?php print $site_name; ?></span>
+                        </a>
+                    <?php endif; ?>
+                
+                    <?php if ($site_slogan): ?>
+                        <h2 class="site-branding__slogan"><?php print $site_slogan; ?></h2>
+                    <?php endif; ?>
                 <?php endif; ?>
             </div>
-            <?php print render($page['navigation']); ?>
-        </div>
-        <div class="l-constrained">
+            
             <?php print render($page['branding']); ?>
         </div>
-        <div class="l-constrained">
-            <?php print render($page['header']); ?>
-        </div>
+            <?php print render($page['header']); ?>            
+            <?php print render($page['navigation']); ?>
     </header>
     
-    <footer class="l-footer-wrapper" role="contentinfo">
+    <div class="l-main">
+        <div class="l-content" role="main">
+            <?php print render($page['highlighted']); ?>
+            <?php print $breadcrumb; ?>
+            <a id="main-content"></a>
+            <?php print render($title_prefix); ?>
+            <?php if ($title): ?>
+                <h1><?php print $title; ?></h1>
+            <?php endif; ?>
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php print render($tabs); ?>
+            <?php print render($page['help']); ?>
+            <?php if ($action_links): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+
+            <?php print render($page['search_tab']); ?>
+            <?php print render($page['showcase']); ?>
+
+            <?php print $feed_icons; ?>
+        </div>
+
+        <?php print render($page['postscript_first']); ?>
+        <?php print render($page['postscript_second']); ?>
+        <?php print render($page['postscript_third']); ?>
+        <?php print render($page['postscript_fourth']); ?>
+
+        <?php print render($page['express']); ?>
+    </div>
+
+    <footer class="l-footer" role="contentinfo">
         <?php print render($page['footer']); ?>
     </footer>
 </div>
