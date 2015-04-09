6a4082b5278090c43a7ab5cf19c431d78a83bdb2
Stephen Foo <stephen.frx@gmail.com>
2015-04-09 22:11:57 +0800

    添加library和sword布局

--- sites/all/themes/gxnulib/template.php
+++ sites/all/themes/gxnulib/template.php
@@ -5,3 +5,8 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * gxnulib theme.
  */
+function gxnulib_omega_layout_alter(&$layout) {
+  if (drupal_is_front_page()) {
+    $layout = 'library';
+  }
+}
